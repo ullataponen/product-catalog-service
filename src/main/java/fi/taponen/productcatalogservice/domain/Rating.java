7ef465ef56c5583160212cc2441d1a1ddd4680f2
Ulla <ulla.taponen@gmail.com>
2021-02-07 13:57:03 +0200

    Rating to include comment

--- src/main/java/fi/taponen/productcatalogservice/domain/Rating.java
+++ src/main/java/fi/taponen/productcatalogservice/domain/Rating.java
@@ -4,14 +4,23 @@ public class Rating {
 	
 	private long productId;
 	private int rating;
+	private String comment;
 	
 	public Rating() {
+
 	}
 
 	public Rating(long productId, int rating) {
 		super();
 		this.productId = productId;
 		this.rating = rating;
+	}
+	
+	public Rating(long productId, int rating, String comment) {
+		super();
+		this.productId = productId;
+		this.rating = rating;
+		this.comment = comment;
 	}
 
 	public long getProductId() {
@@ -29,4 +38,13 @@ public class Rating {
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
 }
